88073fb85ab4ef762aa39098da6f63b4b97d0985
Ricardo Soares <ricardo.sousa@sptech.school>
2023-04-24 13:27:02 -0300

    Animação de digitação.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const titulo = document.getElementById('section1-title')
 
-setInterval(() => {
-  console.log(titulo.innerText)
-  if(titulo.innerText == 'Programação|') titulo.innerText = 'Programação'
-  else titulo.innerText = 'Programação|'
-}, 1000)
+function typeEffect(element, text = '') {
+  element.innerText = ''
+  var i = 0
+  const textInterval = setInterval(() => {
+    if (i < text.length) {
+      element.innerText = text.substring(0, i + 1) + '|'
+      i++
+    }
+  }, 200)
+
+  return setInterval(() => {
+    if(element.innerText == 'Programação' || element.innerText == 'Programação|' ) {
+      clearInterval(textInterval)
+      if(element.innerText == 'Programação|') element.innerText = 'Programação'
+      else element.innerText = 'Programação|'
+    }
+  }, 1000)
+}
+
+const titulo = document.getElementById('section1-title')
+const strProg = 'Programação'
+const intervalTexto = typeEffect(titulo, strProg)
