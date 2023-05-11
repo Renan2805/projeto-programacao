c09ade111aef1c47ad99efdc3d62b61a1d4a7636
Renan2805 <renansilva.dev@gmail.com>
2023-05-10 23:05:59 -0300

    frontedn tela cadastro

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const express = require('express')
 const session = require('express-session')
+const path = require('path')
 const mysql = require('mysql')
 const app = express()
 
@@ -23,7 +24,6 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.use(express.static('public'))
 
 app.use(session({
 	secret: 'secret',
@@ -31,8 +31,19 @@ app.use(session({
 	saveUninitialized: true
 }))
 
+app.use(express.static('public'))
 app.get('/', (_req, res) => {
   res.sendFile('/index.html')
+})
+
+app.use(express.static('public/pages/login'))
+app.get('/login', (_req, res) => {
+  res.sendFile(path.join(__dirname + '/public/pages/login/index.html'))
+})
+
+app.use(express.static('public/pages/cadastro'))
+app.get('/signin', (_req, res) => {
+  res.sendFile(path.join(__dirname + '/public/pages/cadastro/index.html'))
 })
 
 app.post('/auth', (req, res) => {
@@ -41,17 +52,30 @@ app.post('/auth', (req, res) => {
 
   con.query(`select idUsuario, nome, sobrenome, email from usuario join dadosPessoais on idUsuario = fkUsuario where nomeUsuario = '${user}' and senha = '${password}';`, (err, result, fields) => {
     if(err) throw err
-    
+  
     if(result.length > 0) {
       req.session.isLoggedIn = true
-      req.session.user = user
+      req.session.user = {
+        uid: result[0].idUsuario,
+        nome: result[0].nome,
+        sobrenome: result[0].sobrenome,
+        email: result[0].email
+      }
 
-      res.send(req.session)
+      res.redirect('/')
+      console.log(req.session)
+    } else {
+      res.redirect('/login?invalidUser')
     }
+
+    res.end()
 
   })
 })
 
+app.get('/auth', (req, res) => {
+  res.send(req.session)
+})
 
 const PORT = 3000
 http.createServer(app).listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
