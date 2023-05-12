0c2c2bdc9cff3b863a4d9c732815a7b999659565
Renan2805 <renansilva.dev@gmail.com>
2023-05-12 16:07:38 -0300

    Login e cadastro funcionando

--- public/pages/login/script.js
+++ public/pages/login/script.js
@@ -1,5 +1,15 @@
 document.getElementById('btn_login').addEventListener('click', (e) => {
-  fetch('http://localhost/user-data')
+  e.preventDefault()
+  fetch('/usuarios/autenticar', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user: ipt_user.value,
+      password: ipt_password.value
+    })
+  })
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.error(err))
