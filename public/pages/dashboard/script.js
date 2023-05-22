2f266ac6d47bbd04f1e60653651400a3f473539b
Renan2805 <renansilva.dev@gmail.com>
2023-05-22 00:09:55 -0300

    dashboard v1.0

---
+++ public/pages/dashboard/script.js
@@ -0,0 +1,12 @@
+var spans = document.querySelectorAll('.link-side-bar > span')
+
+var sideBar = document.getElementById('side_bar')
+sideBar.addEventListener('mouseenter', () => {
+  sideBar.classList.add('side-bar-open')
+  spans.forEach(span => span.classList.add('side-bar-text-shown'))
+})
+
+sideBar.addEventListener('mouseleave', () => {
+  sideBar.classList.remove('side-bar-open')
+  spans.forEach(span => span.classList.remove('side-bar-text-shown'))
+})
